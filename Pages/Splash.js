8c5aab23c8668f5d8ebbc461e8b29f2a47dd0157
dannertech <danner@danner.tech>
2019-09-23 11:46:09 -0400

    positioned button at the bottom of the Home Screen

--- Pages/Splash.js
+++ Pages/Splash.js
@@ -7,8 +7,10 @@ export default class Splash extends Component {
     render(){
         return(
             <ImageBackground source={require("../Resources/background.png")} style={{width: '100%', height: '100%'}}>
-                <Image style={{width: 60, height: 50}} source={adidasLogo}></Image>
-                <Button color="000" style={styles.button} title="Enter" onPress={() => this.props.navigation.navigate('Search')}></Button>
+                <Image style={{width: 60, height: 50, marginTop: 50}} source={adidasLogo}></Image>
+                <View style={styles.bottom}>
+                <Button color="000" style={{fontSize: 30, marginTop: 200}} title="Enter" onPress={() => this.props.navigation.navigate('Search')}></Button>
+                </View>
             </ImageBackground>
         )
     }
@@ -17,15 +19,9 @@ export default class Splash extends Component {
 
 
 const styles = StyleSheet.create({
-    button : {
-        paddingTop: '120%',
-        paddingLeft: '100%',
-        color: 'black',
-        fontSize: 30
-    },
-    image: {
-        width: 50,
-        height: 50,
-        paddingTop: '50'
-    },
+    bottom: {
+        flex: 1,
+        justifyContent: 'flex-end'
+    }
+    
 })
