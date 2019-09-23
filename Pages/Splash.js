a3f4c2d3aaf14ea1d35c9a7f168016eaa08655a1
dannertech <danner@danner.tech>
2019-09-23 11:03:32 -0400

    successfully changed color to black

--- Pages/Splash.js
+++ Pages/Splash.js
@@ -8,7 +8,7 @@ export default class Splash extends Component {
         return(
             <ImageBackground source={require("../Resources/background.png")} style={{width: '100%', height: '100%'}}>
                 <Image style={{width: 60, height: 50}} source={adidasLogo}></Image>
-                <Button style={styles.button} title="Enter" onPress={() => this.props.navigation.navigate('Search')}></Button>
+                <Button color="000" style={styles.button} title="Enter" onPress={() => this.props.navigation.navigate('Search')}></Button>
             </ImageBackground>
         )
     }
@@ -18,12 +18,14 @@ export default class Splash extends Component {
 
 const styles = StyleSheet.create({
     button : {
-        
-        color: 'black'
+        paddingTop: '120%',
+        paddingLeft: '100%',
+        color: 'black',
+        fontSize: 30
     },
     image: {
         width: 50,
         height: 50,
-        marginTop: '50'
+        paddingTop: '50'
     },
 })
