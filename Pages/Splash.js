886e926dbe2b34e3d7da2a16a50d66b31c43526e
dannertech <danner@danner.tech>
2019-09-23 01:46:15 -0400

    successfully resized image

--- Pages/Splash.js
+++ Pages/Splash.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
-import {View, Text, ImageBackground, Button} from 'react-native';
+import {View, Text, ImageBackground, Button, StyleSheet, Image} from 'react-native';
+
+const adidasLogo = require('../Resources/adidas-logo.png');
 
 export default class Splash extends Component {
     render(){
         return(
             <ImageBackground source={require("../Resources/background.png")} style={{width: '100%', height: '100%'}}>
-                <Text>This is the Splash page</Text>
-                <Button title="Enter" onPress={() => this.props.navigation.navigate('Search')}></Button>
+                <Image style={{width: 60, height: 50}} source={adidasLogo}></Image>
+                <Button style={styles.button} title="Enter" onPress={() => this.props.navigation.navigate('Search')}></Button>
             </ImageBackground>
         )
     }
 }
+
+
+
+const styles = StyleSheet.create({
+    button : {
+        
+        color: 'black'
+    },
+    image: {
+        width: 50,
+        height: 50,
+        marginTop: '50'
+    },
+})
