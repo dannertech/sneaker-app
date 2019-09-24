89402fb29fc0c9d4f34fd72efff9a19228e5232d
dannertech <danner@danner.tech>
2019-09-24 09:28:19 -0400

    styling done for the most part except the textinput in Search, it appears to resize based on what is typed

--- Pages/Results.js
+++ Pages/Results.js
@@ -1,16 +1,50 @@
 import React, { Component } from 'react';
-import {  ListView } from 'react-native';
-import {Card, CardItem, View, Text, Thumbnail, Container, Header, Content, Body} from 'native-base';
+import {  FlatList } from 'react-native';
+import {Card, CardItem, View, Text,Container, Header, Content, Body, Image, Left} from 'native-base';
 
+
+class ProductItem extends React.PureComponent {
+    render(){
+        const item = this.props.item;
+        const url = this.props.item._links.image_small.href
+        return(
+            <Card>
+                <CardItem cardBody>
+                    <Text style={{fontWeight: "bold"}}>{item.original_price}</Text>
+                   </CardItem>
+                   <CardItem>
+                       <Left>
+                           <Text>{item.product_name}</Text>
+                       </Left>
+                   </CardItem>
+                   <CardItem>
+                       <Left>
+                           <Text>{item.description_headline}</Text>
+                       </Left>
+                   </CardItem>
+               </Card>
+        )
+    }
+}
 
 export default class Results extends Component {
+    _keyExtractor = (item, index) => index;
     
+    _renderItem = ({item, index}) => (
+        <ProductItem
+            item={item}
+            index={index}
+        />
+    )
     render(){
         console.log(this.props.navigation.state.params.resultsList)
         return(
-            <View>
-                <Text>This is the Results page</Text>
-            </View>
+            
+               <FlatList style={{marginTop: 50}}
+               data={this.props.navigation.state.params.resultsList}
+               renderItem={this._renderItem}
+            keyExtractor={this._keyExtractor}
+               ></FlatList>        
         )
     }
 }
