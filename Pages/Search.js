d77412350e7f81504e3a27d9649b9f494dd1d6ec
dannertech <danner@danner.tech>
2019-09-24 00:43:31 -0400

    started styling Search page

--- Pages/Search.js
+++ Pages/Search.js
@@ -57,9 +57,9 @@ _performFetchRequest = () => {
         const spinner = this.state.isLoading ? <ActivityIndicator size='large'></ActivityIndicator> : null;
         return(
             <View>
-                <Image source={adidasLogo} style={{width: 100, height: 100}}></Image>
-                <View>
-                <TextInput placeholder="Enter Male or Female" onChange={() => this._onChange(event)}></TextInput>
+                <Image source={adidasLogo} style={{width: 50, height: 50, marginTop: 50}}></Image>
+                <View style={{marginTop: 50}}>
+                <TextInput placeholder="Enter Male or Female" onChange={() => this._onChange(event)} ></TextInput>
                 <Button title="Search" onPress={this._performFetchRequest}/>
                 </View>
                 
